ddf65deaa650803f5a6b9bac82fd241e2907118a
Alexis12119 <corporal461@gmail.com>
2024-11-11 14:07:39 +0800

    chore: cleanup

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,3 @@
-// import 'package:SaliSeek/login_page.dart';
-// import 'package:flutter/material.dart';
-//
-// void main() {
-//   runApp(const MyApp());
-// }
-//
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return const MaterialApp(
-//       home: LoginPage(),
-//     );
-//   }
-// }
-
 import 'package:SaliSeek/login_page.dart';
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
